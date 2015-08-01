8418abb13505c0531e25680d323b5cc39c501d3c
Panashe M. Fundira <fundirap@gmail.com>
2015-08-01 10:44:11 -0400

    Add form partial

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,8 @@ class ArticlesController < ApplicationController
     Article.find(params[:id]).destroy
     redirect_to articles_path
   end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
 end
